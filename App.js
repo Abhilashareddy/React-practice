05b5758d2e234dba95886c35702bf050f18fabca
Abhilashareddy <94732599+Abhilashareddy@users.noreply.github.com>
2024-07-07 11:45:22 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,4 +1,11 @@
-const parent=React.createElement('div',{id:'parent'},[React.createElement('div',{id:'child1'},[React.createElement('h1',{},"i am h1"),React.createElement('h2',{},"i am h2")]),React.createElement('div',{id:'child2'},[React.createElement('h1',{},"i am h1"),React.createElement('h2',{},"i am h2")])])
+const parent=React.createElement('div',{id:'parent'},
+                                 [React.createElement('div',{id:'child1'},
+                                                      [React.createElement('h1',{},"i am h1"),
+                                                       React.createElement('h2',{},"i am h2")]),
+                                  React.createElement('div',{id:'child2'},
+                                                      [React.createElement('h1',{},"i am h1"),
+                                                       React.createElement('h2',{},"i am h2")])
+                                 ])
 
 
 const heading=React.createElement("h1",{id:'heading'},"Hello World from React");  
